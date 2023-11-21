4c043a71a78badb96e1367e2ce8ea81b84511c16
KurymshinaAnn <kurym4@yandex.ru>
2023-11-21 20:08:24 +0100

    date added

--- utils.js
+++ utils.js
@@ -1,23 +1,13 @@
+import format from "date-fns/format";
+
 export const delay = (interval = 300) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, interval);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, interval);
+  });
 };
 
 export const formatDateTime = (date) => {
-    let day = '' + date.getDate();
-    let month = '' + (date.getMonth() + 1);
-    let year = ('' + date.getFullYear()).substring(2);
-    let hours = date.getHours();
-    let minutes = '' + date.getMinutes();
-  
-    if (day.length < 2)
-      day = '0' + day;
-    if (month.length < 2)
-      month = '0' + month;
-    if (minutes.length < 2)
-      minutes = '0' + minutes;
-    return `${day}.${month}.${year} ${hours}:${minutes}`;
-  };
+  return format(date, "yyyy-MM-dd hh.mm.ss");
+};
--- view.js
+++ view.js
@@ -1,9 +1,8 @@
-import { formatDateTime } from './utils.js';
+import { formatDateTime } from "./utils.js";
 
-const listElement = document.getElementById('list');
-const addFormElement = document.getElementById('add-form');
+const listElement = document.getElementById("list");
+const addFormElement = document.getElementById("add-form");
 const authDiv = document.getElementById("auth-form");
-
 
 export const renderAuthForm = () => {
   authDiv.innerHTML = `<div class="add-form-login">
@@ -12,7 +11,7 @@ export const renderAuthForm = () => {
   <input type="password" class="add-form-sign-in-name" id="user-password" placeholder="Пароль">
   <button class="add-form-sign-in-button" id="add-button-login">Войти</button>
   <a class="add-form-sign-up" id="reg-button">Зарегистрироваться</a>
-  </div>`
+  </div>`;
 };
 
 export const renderRegForm = () => {
@@ -23,34 +22,34 @@ export const renderRegForm = () => {
   <input type="password" class="add-form-sign-in-name" id="user-password" placeholder="Введите пароль">
   <button class="add-form-sign-in-button" id="add-button-reg">Зарегистрироваться</button>
   <a class="add-form-sign-up" id="login-button">Войти</a>
-  </div>`
+  </div>`;
 };
 
-export const renderAddForm = (isCreating, user, name = '', text = '') => {
-  if (user === null){
+export const renderAddForm = (isCreating, user, text = "") => {
+  if (user === null) {
     addFormElement.innerHTML = `
     <div class="registration-block">
     <p>Что бы добавить комментарий, <a class="registration-link" id="registration-link">зарегистрируйтесь</a>.</p>
-    </div>`
+    </div>`;
     return;
   }
 
   if (isCreating) {
-      addFormElement.innerHTML = `
+    addFormElement.innerHTML = `
       <div class="add-form-info">
           <p class="add-form-addition">Комментарий загружается</p>
           <div class="add-form-spinner">
               <div class="dots"></div>
           </div>
-      </div>`
+      </div>`;
   } else {
-      addFormElement.innerHTML = `
+    addFormElement.innerHTML = `
       <input type="text" class="add-form-name" id="name-input" placeholder="Введите ваше имя" disabled value="${user.name}"/>
       <textarea type="textarea" class="add-form-text" id="text-input" placeholder="Введите ваш коментарий"
         rows="4">${text}</textarea>
       <div class="add-form-row">
         <button class="add-form-button" id="add-button">Написать</button>
-      </div>`
+      </div>`;
   }
 };
 
@@ -59,10 +58,13 @@ export const renderComments = (comments, isLoading) => {
     listElement.innerHTML = `
         <div class="add-form-info">
             <p class="add-form-addition">Пожалуйста подождите, загружаем комментарии</p>
-        </div>`
+        </div>`;
   } else {
-    const commentsHtml = comments.map((comment, index) => {
-      return `<li class="comment" data-name="${comment.name}" data-text="${comment.text}">
+    const commentsHtml = comments
+      .map((comment, index) => {
+        return `<li class="comment" data-name="${comment.name}" data-text="${
+          comment.text
+        }">
         <div class="comment-header">
           <div>${comment.name}</div>
           <div>${formatDateTime(comment.time)}</div>
@@ -72,31 +74,36 @@ export const renderComments = (comments, isLoading) => {
         </div>
         <div class="comment-footer">
           <div class="comment-button">
-            <button class="comment-button-edit ${comment.isEdit ? "comment-button-edit-save" : ""}" 
+            <button class="comment-button-edit ${
+              comment.isEdit ? "comment-button-edit-save" : ""
+            }" 
             id="comment-button-edit" data-index=${index}>
             ${comment.isEdit ? "Сохранить" : "Редактировать"}
             </button>
           </div>
           <div class="likes">
             <span class="likes-counter">${comment.likes}</span>
-            <button class="like-button ${comment.liked ? "-active-like" : ""}" data-index="${index}"></button>
+            <button class="like-button ${
+              comment.liked ? "-active-like" : ""
+            }" data-index="${index}"></button>
           </div>
         </div>
-      </li>`
-    }).join('');
+      </li>`;
+      })
+      .join("");
     listElement.innerHTML = commentsHtml;
   }
 };
 
 const renderCommentsText = (comment) => {
   if (comment.isEdit) {
-    return `<textarea class="comment-text-edit" id="edit-textarea" value="">${comment.text}</textarea>`
+    return `<textarea class="comment-text-edit" id="edit-textarea" value="">${comment.text}</textarea>`;
   } else {
     const renderedText = comment.text
       .replaceAll("QUOTE_BEGIN", "<div class='quote'>")
       .replaceAll("QUOTE_END", "</div>");
     return `<div class="comment-text" data-text="${comment.text}">
       ${renderedText}
-      </div>`
+      </div>`;
   }
 };
